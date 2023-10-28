3c7273a1fe3d23b7d9f54f6055a3f5c79d386bae
rizwan-signon <rizwan.signon@gmail.com>
2023-10-28 23:17:08 +0500

    changes done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import "./style.css";
 import { useState } from "react";
 import Todoapp from "./Todoapp";
 import Simpletodo from "./Simpletodo";
-import Title from "./Title";
 
 export default function App() {
   return (
     <div>
-      <Title />
       <Todoapp />
       {/* <Simpletodo /> */}
     </div>
--- src/Title.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export default function Title() {
-  return (
-    <div>
-      <h1>this is simple to do list by Rizwan hameed </h1>
-    </div>
-  );
-}
